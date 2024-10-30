8de759ab8a851e711bd8f8432f1ceb48ef776b06
RobertJN64 <robertjnies@gmail.com>
2024-10-30 02:30:24 -0400

    IT WORKS

---
+++ app/src/main/java/com/example/kotlin_nfc_ev/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.kotlin_nfc_ev
+
+import android.nfc.NfcAdapter
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val dataStore = DataStoreUtil(this)
+        val uid = dataStore.getID()
+        val textView: TextView = findViewById(R.id.textID)
+        textView.text = uid
+
+        val nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+        if (nfcAdapter != null) {
+            if (!nfcAdapter.isEnabled) {
+                NFCDialog(this).showNFCDisabled()
+            }
+        } else{
+            NFCDialog(this).showNFCUnsupported()
+        }
+
+
+
+
+    }
+
+}
